37eebb07bdcc85f2a5ad332430d13bf352a01569
אביאל <x0583289789@gmail.com>
2025-08-14 16:44:38 +0300

    Update a.py

--- a.py
+++ a.py
@@ -22,6 +22,8 @@ def flask_controller():
 # B
 def yemot_service(request_params):
     x = request_params.get("x")
+    if x == 1:
+        a()
     #
     #
     #
@@ -31,7 +33,6 @@ def yemot_service(request_params):
     #
     #
     #
-    #
 
 
 
@@ -43,6 +44,27 @@ def yemot_service(request_params):
 
 
 
-# C
-def core_logic():
-    pass
+#מחשבון
+def a():
+    print("-t ברוך הבא למחשבון, אנא הקש את המספר הראשון record_digits=yes")
+    a = digits + 0
+    print("-t אנא הקש פעולה, חיבור,1, חיסור, 2, כפל, 3, חילוק, 4,   record_digits=yes")
+    b = digits + 0
+    print("-t אנא הקש את המספר השני record_digits=yes")
+    c = digits + 0
+    if b == 1:
+        d = a + c
+    elif b == 2:
+        d = a - c
+    elif b == 3:
+        d = a * c
+    elif b == 4:
+        if b != 0:
+            d = a = c
+        else:
+            d = "אי אפשר לחלק באפס"
+    else:
+        d = "פעולה לא חוקית"
+    print(f"-t התוצאה היא:{d}")
+
+
